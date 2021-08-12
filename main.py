49ce8218afa91eea8420bbe1c43e6c762adf02b6
cclineCPG <charliecline@counterplay.co>
2021-08-12 13:06:37 -0500

    Code for generating a patch

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import subprocess
+import posixpath
+
+
+def generate_patch_file(target):
+    subprocess.call(['git', 'fetch', 'origin', target])
+    old_patch = subprocess.check_output(['git', 'format-patch', f'origin/{target}', 'fld/.', '-o', 'patches'])
+    old_patch = old_patch.decode('utf-8')
+
+    patch_path = os.path.dirname(old_patch)
+    patch_file = os.path.basename(old_patch)
+
+    split = patch_file.split('-')
+    patch_file = ''.join(split[1:])
+    new_patch = posixpath.join(patch_path, patch_file)
+
+    os.rename(old_patch.rstrip('\n'), new_patch.rstrip('\n'))
+
+    with open('patches/.patches', 'r+') as f:
+        lines = f.readlines()
+        lines.insert(0, patch_file)
+        f.seek(0)
+        f.writelines(lines)
+
+    return new_patch
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    parent_branch = os.environ["INPUT_PARENT_BRANCH"]
 
-    my_output = f"Hello {my_input}"
-
-    print(f"::set-output name=myOutput::{my_output}")
+    out_patch_file = generate_patch_file(parent_branch)
+    print(f"::set-output name=out_patch_file::{out_patch_file}")
 
 
 if __name__ == "__main__":
